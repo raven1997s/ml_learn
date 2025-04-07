1d32192138f9cfc5c81a4eec7f248121a4c32e8a
raven <gaoning176@gmail.com>
2025-04-07 10:26:48 +0800

    gpu

--- PyTorch_Learn/model.py
+++ PyTorch_Learn/model.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import Conv2d, MaxPool2d, Flatten, Linear
+import torch.nn as nn
 
 
 # 搭建神经网络
@@ -8,13 +9,17 @@ class MyModule(torch.nn.Module):
         super(MyModule, self).__init__()
         self.model = torch.nn.Sequential(
             Conv2d(3, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
             MaxPool2d(kernel_size=2),
             Conv2d(32, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
             MaxPool2d(kernel_size=2),
             Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
             MaxPool2d(kernel_size=2),
             Flatten(),
             Linear(64 * 4 * 4, 64),
+            nn.ReLU(),
             Linear(64, 10),  # CIFAR10有10个类别
         )
 
